c16ac79fa458d35aad46c39b7918db5c7cf29f66
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-07 10:04:21 +0300

    Initial commit

---
+++ src/main/java/by/smirnov/gitpracticeproject/controller/TelegramBot.java
@@ -0,0 +1,27 @@
+package by.smirnov.gitpracticeproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
---
+++ src/test/java/by/smirnov/gitpracticeproject/GitPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.smirnov.gitpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
