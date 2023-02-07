170cb5416932cdf83d3a53241afcd488f18fa9c3
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-07 13:58:05 +0300

    Add User entity

---
+++ src/main/java/by/smirnov/gitpracticeproject/entity/User.java
@@ -0,0 +1,38 @@
+package by.smirnov.gitpracticeproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "tgbot")
+public class User {
+
+    @Id
+    @Column(name="chat_id")
+    private Long chatId;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name="registered_at")
+    private Timestamp registeredAt;
+}
