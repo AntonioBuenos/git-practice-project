ba3dde0505f798041d03de14df4ae96a88c70642
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-07 15:53:50 +0300

    Optimize repositories, create AdService

--- src/main/java/by/smirnov/gitpracticeproject/repository/AdRepository.java
+++ src/main/java/by/smirnov/gitpracticeproject/repository/AdRepository.java
@@ -1,7 +1,9 @@
 package by.smirnov.gitpracticeproject.repository;
 
 import by.smirnov.gitpracticeproject.entity.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AdRepository extends CrudRepository<Ad, Long> {
+public interface AdRepository extends CrudRepository<Ad, Long>,
+        JpaRepository<Ad, Long> {
 }
--- src/main/java/by/smirnov/gitpracticeproject/repository/UserRepository.java
+++ src/main/java/by/smirnov/gitpracticeproject/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package by.smirnov.gitpracticeproject.repository;
 
 import by.smirnov.gitpracticeproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>,
+        JpaRepository<User, Long> {
 }
