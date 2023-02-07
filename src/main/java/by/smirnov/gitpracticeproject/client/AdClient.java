29cd5f45ab2ea406e82070d9229825bf945241f1
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-07 17:23:42 +0300

    Add AdClient

---
+++ src/main/java/by/smirnov/gitpracticeproject/client/AdClient.java
@@ -0,0 +1,10 @@
+package by.smirnov.gitpracticeproject.client;
+
+import by.smirnov.gitpracticeproject.entity.Ad;
+
+import java.util.List;
+
+public interface AdClient {
+
+    List<Ad> getAds();
+}
